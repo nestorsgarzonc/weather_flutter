2fc6130843a397769f5633477ee27ea676d996c2
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-15 20:18:16 -0500

    adding files

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
+
+final kTempTextStyle=GoogleFonts.amiko(fontSize: 100);
+final kMessageTextStyle=GoogleFonts.amiko(fontSize: 60);
+final kButtonTextStyle=GoogleFonts.amiko(fontSize: 30);
+final kConditionTextStyle=GoogleFonts.amiko(fontSize: 100);
